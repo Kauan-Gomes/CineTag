69b9ff4dc9608795a3d8d13996db63f51751d6cc
Kauan Gomes <Kauang13@gmail.com>
2023-08-03 18:27:43 -0300

    context criado

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Favoritos from "pages/Favoritos";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Container from "components/Container";
+import FavoritosProvider from "context/favorito";
 
 
 export default function AppRoutes() {
@@ -12,10 +13,12 @@ export default function AppRoutes() {
         <BrowserRouter>
             <Cabecalho />
             <Container>
-                <Routes>
-                    <Route path="/" element={<Start />}></Route>
-                    <Route path="/favoritos" element={<Favoritos />}></Route>
-                </Routes>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Start />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritosProvider>
             </Container>
             <Rodape />
         </BrowserRouter>
